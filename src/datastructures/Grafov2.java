02ff7591d92967f9a162fd12553ede22709dc95c
Samuel Satizabal <samuelsatizabal@hotmail.com>
2020-05-01 17:14:30 -0500

    guardando copia de seguridad (otra vez xD)

--- src/datastructures/Grafov2.java
+++ src/datastructures/Grafov2.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class Grafov2<T> {
 	private ArrayList<Integer>[][] adjmatrix;
-//	private ArrayList<ArrayList<ArrayList<Integer>>> asd;
+	private ArrayList<ArrayList<ArrayList<Integer>>> asd;
 	private boolean directed;
 	private ArrayList<T> values;
 	private boolean multiple;
@@ -14,20 +14,23 @@ public class Grafov2<T> {
 	
 	public Grafov2(boolean directed, int i, boolean multiple) {
 		
-//		asd = new ArrayList<ArrayList<ArrayList<Integer>>>();
-//		asd.get(0).get(0).get(0)
-		
 		adjmatrix = new ArrayList[i][i];
 		this.directed = directed;
 		this.multiple = multiple;
 		values = new ArrayList<>();
 		
+		asd = new ArrayList<ArrayList<ArrayList<Integer>>>();
+		
 		for(int j = 0; j < i; j++) {
+			asd.addAll(new ArrayList<ArrayList<ArrayList<Integer>>>());
 			for(int k = 0; k < i; k++) {
+				asd.get(j).addAll(new ArrayList<ArrayList<Integer>>());
 				adjmatrix[j][k] = new ArrayList<Integer>();
 			}
 		}
 	}
+	
+	
 	
 	/*
 	 * i = origin node
@@ -35,31 +38,51 @@ public class Grafov2<T> {
 	 * w = weight
 	 */
 	public void addEdge(int i, int j, int w) {
+//		if(multiple) {
+//			addEdgeAux(i,j,w);
+//		}else {
+//			if(adjmatrix[i][j].isEmpty()) {
+//				addEdgeAux(i,j,w);
+//			}
+//		}
+		
 		if(multiple) {
 			addEdgeAux(i,j,w);
 		}else {
-			if(adjmatrix[i][j].isEmpty()) {
+			if(asd.get(i).get(j).isEmpty()) {
 				addEdgeAux(i,j,w);
 			}
 		}
+		
 	}
 	
 	public void addEdgeAux(int i, int j, int w) {
-		adjmatrix[i][j].add(w);
+//		adjmatrix[i][j].add(w);
+//		if(!directed && j!=i) {
+//			adjmatrix[j][i].add(w);
+//		}
+		
+		asd.get(i).get(j).add(w);
 		if(!directed && j!=i) {
-			adjmatrix[j][i].add(w);
+			asd.get(j).get(i).add(w);
 		}
 	}
 	
 	public void addVertex(T v) {
-		int x = adjmatrix.length+1;
-		ArrayList<Integer>[][] aux = new ArrayList[x][x];
-		for(int j = 0; j < x; j++) {
-			for(int k = 0; k < x; k++) {
-				aux[j][k] = new ArrayList<Integer>();
-			}
+//		int x = adjmatrix.length+1;
+//		ArrayList<Integer>[][] aux = new ArrayList[x][x];
+//		for(int j = 0; j < x; j++) {
+//			for(int k = 0; k < x; k++) {
+//				aux[j][k] = new ArrayList<Integer>();
+//			}
+//		}
+//		adjmatrix = aux;
+		
+		asd.add(new ArrayList<ArrayList<Integer>>());
+		for(int i = 0; i < asd.size(); i++) {
+			asd.get(i).add(new ArrayList<Integer>());
 		}
-		adjmatrix = aux;
+				
 		values.add(v);
 	}
 	
@@ -73,20 +96,22 @@ public class Grafov2<T> {
 	 * w = weight
 	 */
 	public void deleteEdge(int i, int j, int w) {
-		if(!adjmatrix[i][j].isEmpty()) {
-			for(int x = 0; x < adjmatrix[i][j].size(); x++) {
-				if(adjmatrix[i][j].get(x)==w) {
-					adjmatrix[i][j].remove(x);
-				}
-				if(!isDirected()) {
-					if(adjmatrix[j][i].get(x)==w) {
-						adjmatrix[j][i].remove(x);
-					}
-				}
-				
-			}
-			
-		}
+//		if(!adjmatrix[i][j].isEmpty()) {
+//			for(int x = 0; x < adjmatrix[i][j].size(); x++) {
+//				if(adjmatrix[i][j].get(x)==w) {
+//					adjmatrix[i][j].remove(x);
+//				}
+//				if(!isDirected()) {
+//					if(adjmatrix[j][i].get(x)==w) {
+//						adjmatrix[j][i].remove(x);
+//					}
+//				}
+//				
+//			}
+//			
+//		}
+		
+		
 	}
 	
 	public void deleteVertex(T v) {
