08ecea145427d052d9c67e079e7dc65cf2452416
Samuel Satizabal <samuelsatizabal@hotmail.com>
2020-05-02 09:41:07 -0500

    Alpha v1.1

--- src/datastructures/Grafov2.java
+++ src/datastructures/Grafov2.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class Grafov2<T> {
 	private ArrayList<Integer>[][] adjmatrix;
-	private ArrayList<ArrayList<ArrayList<Integer>>> asd;
+//	private ArrayList<ArrayList<ArrayList<Integer>>> asd;
 	private boolean directed;
 	private ArrayList<T> values;
 	private boolean multiple;
@@ -19,12 +19,12 @@ public class Grafov2<T> {
 		this.multiple = multiple;
 		values = new ArrayList<>();
 		
-		asd = new ArrayList<ArrayList<ArrayList<Integer>>>();
+//		asd = new ArrayList<ArrayList<ArrayList<Integer>>>();
 		
 		for(int j = 0; j < i; j++) {
-			asd.addAll(new ArrayList<ArrayList<ArrayList<Integer>>>());
+//			asd.addAll(new ArrayList<ArrayList<ArrayList<Integer>>>());
 			for(int k = 0; k < i; k++) {
-				asd.get(j).addAll(new ArrayList<ArrayList<Integer>>());
+//				asd.get(j).addAll(new ArrayList<ArrayList<Integer>>());
 				adjmatrix[j][k] = new ArrayList<Integer>();
 			}
 		}
@@ -38,50 +38,50 @@ public class Grafov2<T> {
 	 * w = weight
 	 */
 	public void addEdge(int i, int j, int w) {
-//		if(multiple) {
-//			addEdgeAux(i,j,w);
-//		}else {
-//			if(adjmatrix[i][j].isEmpty()) {
-//				addEdgeAux(i,j,w);
-//			}
-//		}
-		
 		if(multiple) {
 			addEdgeAux(i,j,w);
 		}else {
-			if(asd.get(i).get(j).isEmpty()) {
+			if(adjmatrix[i][j].isEmpty()) {
 				addEdgeAux(i,j,w);
 			}
 		}
 		
+//		if(multiple) {
+//			addEdgeAux(i,j,w);
+//		}else {
+//			if(asd.get(i).get(j).isEmpty()) {
+//				addEdgeAux(i,j,w);
+//			}
+//		}
+		
 	}
 	
 	public void addEdgeAux(int i, int j, int w) {
-//		adjmatrix[i][j].add(w);
-//		if(!directed && j!=i) {
-//			adjmatrix[j][i].add(w);
-//		}
-		
-		asd.get(i).get(j).add(w);
+		adjmatrix[i][j].add(w);
 		if(!directed && j!=i) {
-			asd.get(j).get(i).add(w);
+			adjmatrix[j][i].add(w);
 		}
+		
+//		asd.get(i).get(j).add(w);
+//		if(!directed && j!=i) {
+//			asd.get(j).get(i).add(w);
+//		}
 	}
 	
 	public void addVertex(T v) {
-//		int x = adjmatrix.length+1;
-//		ArrayList<Integer>[][] aux = new ArrayList[x][x];
-//		for(int j = 0; j < x; j++) {
-//			for(int k = 0; k < x; k++) {
-//				aux[j][k] = new ArrayList<Integer>();
-//			}
-//		}
-//		adjmatrix = aux;
-		
-		asd.add(new ArrayList<ArrayList<Integer>>());
-		for(int i = 0; i < asd.size(); i++) {
-			asd.get(i).add(new ArrayList<Integer>());
+		int x = adjmatrix.length+1;
+		ArrayList<Integer>[][] aux = new ArrayList[x][x];
+		for(int j = 0; j < x; j++) {
+			for(int k = 0; k < x; k++) {
+				aux[j][k] = new ArrayList<Integer>();
+			}
 		}
+		adjmatrix = aux;
+		
+//		asd.add(new ArrayList<ArrayList<Integer>>());
+//		for(int i = 0; i < asd.size(); i++) {
+//			asd.get(i).add(new ArrayList<Integer>());
+//		}
 				
 		values.add(v);
 	}
@@ -96,20 +96,20 @@ public class Grafov2<T> {
 	 * w = weight
 	 */
 	public void deleteEdge(int i, int j, int w) {
-//		if(!adjmatrix[i][j].isEmpty()) {
-//			for(int x = 0; x < adjmatrix[i][j].size(); x++) {
-//				if(adjmatrix[i][j].get(x)==w) {
-//					adjmatrix[i][j].remove(x);
-//				}
-//				if(!isDirected()) {
-//					if(adjmatrix[j][i].get(x)==w) {
-//						adjmatrix[j][i].remove(x);
-//					}
-//				}
-//				
-//			}
-//			
-//		}
+		if(!adjmatrix[i][j].isEmpty()) {
+			for(int x = 0; x < adjmatrix[i][j].size(); x++) {
+				if(adjmatrix[i][j].get(x)==w) {
+					adjmatrix[i][j].remove(x);
+				}
+				if(!isDirected()) {
+					if(adjmatrix[j][i].get(x)==w) {
+						adjmatrix[j][i].remove(x);
+					}
+				}
+				
+			}
+			
+		}
 		
 		
 	}
@@ -122,7 +122,19 @@ public class Grafov2<T> {
 				values.remove(x);
 			}
 		}
-		//unfinished
+		int m = n;
+		
+		if(n!=-1) {
+			for(;n < adjmatrix.length; n++) {
+				for(;m < adjmatrix[n].length; m++) {
+					adjmatrix[n][m] = adjmatrix[n][m+1];
+				}
+				adjmatrix[n] = adjmatrix[n+1];
+			}
+		}
+		ArrayList<Integer>[][] aux = new ArrayList[adjmatrix.length-1][adjmatrix.length-1];
+		aux = adjmatrix;
+		adjmatrix = aux;
 	}
 	
 	public int consultWeight() {
