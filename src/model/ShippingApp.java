08718692065fef5d59d1e8463bf432150c933208
JuanOssa1 <juan.ossa@hotmail.com>
2020-05-16 20:56:51 -0500

    1.38

--- src/model/ShippingApp.java
+++ src/model/ShippingApp.java
@@ -105,6 +105,9 @@ public class ShippingApp {
 		}
     	return countrysName;  
     }
+    public void covidMode() {
+    	countrys = countrys.kruskal(); 	
+    }
     public void addBoats() {
 		Country china = new Country("china", 002123);
 		Country usa = new Country("usa", 002123);
@@ -113,29 +116,48 @@ public class ShippingApp {
 		Country rusia = new Country("rusia", 002123);
 		Country southcorea = new Country("southcorea", 002123);
 		Country australia = new Country("australia", 002123);
-		
-		china.addBoat("Titanic ll","2003", 12, 38);
-		china.addBoat("Zombies Cant swim","2012",12.3,32.3);
-		china.addBoat("Breaking Bass","2018",12.7,33);
-		usa.addBoat("Codfather","1999",14,38.3);
-		usa.addBoat("Kobe Boat","2020",13,42.3);
-		usa.addBoat("Pug Boat","2017",16,31.3);
-		jamaica.addBoat("Usain Boat","1986",7.4,65);
-		jamaica.addBoat("Error 404 fish not found","1969",7.4,35);
-		jamaica.addBoat("The wet dream","2004",7.4,63);
-		brasil.addBoat("Vitamin Sea","1972",10,45.3);
-		brasil.addBoat("Kayot","1990",10,47.3);
-		brasil.addBoat("Favorite Mistake","2000",10,47.3);
-		rusia.addBoat("Liquid Asset","1991",13.9,42.3);
-		rusia.addBoat("Vesper","1991",13.9,39.3);
-		rusia.addBoat("Unsinkable ll","1991",13.9,41.3);
-		southcorea.addBoat("Nayeon ","1995",25.5,50);
-		southcorea.addBoat("Twice","2015",40.5,43.3);
-		southcorea.addBoat("Dahyun","1998",25.5,42);
-		australia.addBoat("Villa Cubito Boat","2020",21.3,41.3);
-		australia.addBoat("Golder of the sea","2020",21.3,39.3);
-		australia.addBoat("Samsattas","2020",21.3,60.3);
-		
+		china.addBoat("Titanic ll","2003", 12000, 38);
+		china.addBoat("Zombies Cant swim","2012",12300,32.3);
+		china.addBoat("Breaking Bass","2018",12700,33);
+		usa.addBoat("Codfather","1999",14000,38.3);
+		usa.addBoat("Kobe Boat","2020",13000,42.3);
+		usa.addBoat("Pug Boat","2017",16000,31.3);
+		jamaica.addBoat("Usain Boat","1986",7400,65);
+		jamaica.addBoat("Error 404 fish not found","1969",7400,35);
+		jamaica.addBoat("The wet dream","2004",7400,63);
+		brasil.addBoat("Vitamin Sea","1972",10000,45.3);
+		brasil.addBoat("Kayot","1990",10000,47.3);
+		brasil.addBoat("Favorite Mistake","2000",10000,47.3);
+		rusia.addBoat("Liquid Asset","1991",13900,42.3);
+		rusia.addBoat("Vesper","1991",13900,39.3);
+		rusia.addBoat("Unsinkable ll","1991",13900,41.3);
+		southcorea.addBoat("Nayeon ","1995",25500,50);
+		southcorea.addBoat("Twice","2015",40500,43.3);
+		southcorea.addBoat("Dahyun","1998",25500,42);
+		australia.addBoat("Villa Cubito Boat","2020",21300,41.3);
+		australia.addBoat("Golder of the sea","2020",21300,39.3);
+		australia.addBoat("Samsattas","2020",21300,60.3);
+		countrys.addEdge(china, usa, 10394);
+		countrys.addEdge(china, jamaica, 18321);
+		countrys.addEdge(china, brasil, 21443);
+		countrys.addEdge(china, rusia, 2065);
+		countrys.addEdge(china, southcorea, 780);
+		countrys.addEdge(china, australia, 6923);
+		countrys.addEdge(usa, rusia, 8513);
+		countrys.addEdge(usa, jamaica, 7514);
+		countrys.addEdge(usa, brasil, 10781);
+		countrys.addEdge(usa, southcorea, 9682);
+		countrys.addEdge(usa, australia, 11243);
+		countrys.addEdge(jamaica, brasil, 3724);
+		countrys.addEdge(jamaica, rusia, 16189);
+		countrys.addEdge(jamaica, southcorea, 17766);
+		countrys.addEdge(jamaica, australia, 16457);
+		countrys.addEdge(brasil, rusia, 22782);
+		countrys.addEdge(brasil, southcorea, 21147);
+		countrys.addEdge(brasil, australia, 19534);
+		countrys.addEdge(rusia, southcorea, 1378);
+		countrys.addEdge(rusia, australia, 8545);
+		countrys.addEdge(southcorea, australia, 7325);
 		countrys.addVertex(china);
 		countrys.addVertex(usa);
 		countrys.addVertex(jamaica);
@@ -144,6 +166,4 @@ public class ShippingApp {
 		countrys.addVertex(southcorea);
 		countrys.addVertex(australia);
 	}
-
-	
 }
