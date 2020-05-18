7cb856ee1d0c5b539b5198c897a5e04c2cfbdebc
JuanOssa1 <juan.ossa@hotmail.com>
2020-05-17 21:06:49 -0500

    Final v1.0

--- src/test/ShippingAppTest.java
+++ src/test/ShippingAppTest.java
@@ -59,16 +59,7 @@ class ShippingAppTest {
 	@Test
 	void testgetCountryValue() {
 		ShippingApp shipping = setUpSceneGetCountryValue();
-		try {
-			assertEquals(7,shipping.deliveryTime("australia", "usa"));
-			shipping.setCountrys(shipping.getCountrys().kruskal());
-			assertEquals(17,shipping.deliveryTime("australia", "usa"));
-			
-		} catch (UnavaiableBoatsException e) {
-			
-		} catch (MaximumRangeExceededException e) {
-		
-		}
+		assertEquals("Country [name=china, id=1107, boats=[Boat [name=Titanic ll, manufacturingDate=2003, maxRange=12000.0, maxSpeed=38.0], Boat [name=Zombies Cant swim, manufacturingDate=2012, maxRange=12300.0, maxSpeed=32.3], Boat [name=Breaking Bass, manufacturingDate=2018, maxRange=12700.0, maxSpeed=33.0]]]",shipping.getCountryValue("china").toStringB());
 	}
 	
 
